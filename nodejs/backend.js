b82d537dbed53e88ed720290a9fa78f02a96f58c
Junho <poiewq1731@gmail.com>
2018-11-15 11:12:22 +0900

    mongodb connect

--- nodejs/backend.js
+++ nodejs/backend.js
@@ -1,1 +1,14 @@
-var MongoClient = require('mongodb');
+// 1. mongoose 모듈 가져오기
+var mongoose = require('mongoose');
+// 2. testDB 세팅
+mongoose.connect('mongodb://localhost:27017');
+// 3. 연결된 testDB 사용
+var db = mongoose.connection;
+// 4. 연결 실패
+db.on('error', function(){
+    console.log('Connection Failed!');
+});
+// 5. 연결 성공
+db.once('open', function() {
+    console.log('Connected!');
+});
